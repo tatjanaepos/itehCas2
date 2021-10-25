606654420feb3c452bd526f4d36d5a8b7d75d7ec
Tatjana Parezanovic <tatjana.parezanovic@gmail.com>
2021-10-25 22:04:42 +0200

    Inicijalizacija kostura

---
+++ index.php
@@ -0,0 +1,64 @@
+<?php
+
+require "dbBroker.php";
+require "model/user.php";
+
+session_start();
+if(isset($_POST['username']) && isset($_POST['password'])){
+    $uname = $_POST['username'];
+    $upass = $_POST['password'];
+
+    // $conn = new mysqli() /// pregazena konekcija iz dbBrokera;
+    $korisnik = new User(1, $uname, $upass);
+    // $odg = $korisnik->logInUser($uname, $upass, $conn);
+    $odg = User::logInUser($korisnik, $conn); //pristup statickim funkcijama preko klase
+
+    if($odg->num_rows==1){
+        echo  `
+        <script>
+        console.log( "Uspešno ste se prijavili");
+        </script>
+        `;
+        $_SESSION['user_id'] = $korisnik->id;
+        header('Location: home.php');
+        exit();
+    }else{
+        echo `
+        <script>
+        console.log( "Niste se prijavili!");
+        </script>
+        `;
+    }
+
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+    <title>FON: Zakazivanje kolokvijuma</title>
+
+</head>
+<body>
+    <div class="login-form">
+        <div class="main-div">
+            <form method="POST" action="#">
+                <div class="container">
+                    <label class="username">Korisnicko ime</label>
+                    <input type="text" name="username" class="form-control"  required>
+                    <br>
+                    <label for="password">Lozinka</label>
+                    <input type="password" name="password" class="form-control" required>
+                    <button type="submit" class="btn btn-primary" name="submit">Prijavi se</button>
+                </div>
+
+            </form>
+        </div>
+
+        
+    </div>
+</body>
+</html>
